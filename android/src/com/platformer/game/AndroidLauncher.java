4a1d8693fca22d80f52148c50fe53902480517f7
aclaputra <muhamadacla22@gmail.com>
2022-10-22 16:12:39 +0700

    feat: initial commit

---
+++ android/src/com/platformer/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.platformer.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.platformer.game.RNBPlatformer;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RNBPlatformer(), config);
+	}
+}
